0f1e475172918e93fc8ecd4b1d98a9a2e8f197a6
codyalvarez <jedi.alvarez805@gmail.com>
2022-04-14 11:24:57 -0400

    first commit

---
+++ guess.py
@@ -0,0 +1,9 @@
+guess = int(input("What is your guess?: "))
+correct_number = 5
+guess_count = 0
+
+while guess != correct_number:
+    guess_count += 1
+    guess = int(input("What is your guess?: "))
+
+print(f"Congrats! The right answer was {correct_number} It took you {guess_count} guesses you piece of shit. Fuck you.")
